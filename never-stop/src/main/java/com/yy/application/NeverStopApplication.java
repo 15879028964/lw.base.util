c03671c0da35f62633b54ffa7e51e9b24af2801e
David <liuwei01@dongfangfuli.com>
2020-05-07 14:56:16 +0800

    新增项目

---
+++ never-stop/src/main/java/com/yy/application/NeverStopApplication.java
@@ -0,0 +1,13 @@
+package com.yy.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeverStopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeverStopApplication.class, args);
+	}
+
+}
